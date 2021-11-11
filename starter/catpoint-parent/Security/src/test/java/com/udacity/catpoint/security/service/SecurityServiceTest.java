3e9ec8c9e0c99f6e5b0d1ec1f688c58a8d0f1eab
OualidKassa <Shoananas94@>
2021-11-12 00:23:26 +0100

    Correction pom.xml parent -> remove dependency junit-jupiter-engine for tests work!

--- starter/catpoint-parent/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
+++ starter/catpoint-parent/Security/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
@@ -5,6 +5,7 @@ import com.udacity.catpoint.security.application.StatusListener;
 import com.udacity.catpoint.security.data.AlarmStatus;
 import com.udacity.catpoint.security.data.ArmingStatus;
 import com.udacity.catpoint.security.data.SecurityRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,8 +35,14 @@ public class SecurityServiceTest {
     }
 
     @Test
-    void ShouldCallSecurityRepositoryWhenChangeArmingStatus(){
+    public void ShouldCallSecurityRepositoryWhenChangeArmingStatus(){
         securityService.setArmingStatus(ArmingStatus.DISARMED);
         Mockito.verify(securityRepository, Mockito.times(1)).setArmingStatus(ArmingStatus.DISARMED);
+    }
+
+    @Test
+    public void test(){
+        int a = 5;
+        Assertions.assertEquals(5, a);
     }
 }
